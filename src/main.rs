4962ce136b3bcb729576a4b6b52deba734d23dbb
Gerd Zellweger <mail@gerdzellweger.com>
2018-03-28 16:03:55 -0700

    Update code.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,79 @@
 extern crate mio;
+extern crate time;
+extern crate byteorder;
+extern crate csv;
+
+#[macro_use]
+extern crate serde_derive;
+
+
 use std::env;
 use std::time::Duration;
+use csv::Writer;
 
 use mio::net::UdpSocket;
 use mio::{Events, Ready, Poll, PollOpt, Token};
 
+use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
+
 //use {expect_events, sleep_ms};
 
+#[derive(Serialize)]
+struct Row {
+    latency_ns: u64,
+}
+
 const PING: Token = Token(0);
-const PONG: Token = Token(1);
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
     assert!(args.len() >= 2);
     println!("Connecting to {}", args[1]);
+    let sender = UdpSocket::bind(&"0.0.0.0:1111".parse().expect("Invalid address.")).expect("Can't bind");
 
-    let sender = UdpSocket::bind(&args[1].parse().unwrap()).unwrap();
+    sender.connect(args[1].parse().expect("Invalid host:port pair")).expect("Can't connect to server");
     let poll = Poll::new().expect("Can't create poll.");
 
-    poll.register(&sender, PING, Ready::writable(), PollOpt::edge()).expect("Can't register send event.");
-    poll.register(&sender, PONG, Ready::readable(), PollOpt::edge()).expect("Can't register receive event.");
+    poll.register(&sender, PING, Ready::writable() | Ready::readable(), PollOpt::level()).expect("Can't register send event.");
 
-    let mut buffer = [1; 9];
-    let msg_to_send = [9; 9];
-
+    let mut buf = Vec::with_capacity(8);
     let mut events = Events::with_capacity(1024);
+
+    let mut recv_buf: Vec<u8> = Vec::with_capacity(8);
+    recv_buf.resize(8, 0);
+
+    let mut wtr = Writer::from_path("latency.csv").expect("Can't open log file for writing");
+    
+    let mut i = 0;
+    let mut waiting_for_reply = false;
     loop {
         poll.poll(&mut events, Some(Duration::from_millis(100))).expect("Can't poll channel");
         for event in events.iter() {
-            match event.token() {
+ 
+            if  !waiting_for_reply && event.readiness().is_writable() {
+                buf.write_u64::<BigEndian>(time::precise_time_ns()).expect("Serialize time");
+                let bytes_sent = sender.send(&buf).expect("Sending failed!");
+                assert_eq!(bytes_sent, 8);
+                buf.clear();
+                waiting_for_reply = true;
+            }
 
-                // Our SENDER is ready to be written into.
-                PING => {
-                    let bytes_sent = sender.send(&msg_to_send).expect("Sending failed");
-                    assert_eq!(bytes_sent, 9);
-                    println!("sent {:?} -> {:?} bytes", msg_to_send, bytes_sent);
-                },
+            if waiting_for_reply && event.readiness().is_readable() {
+                let _ = sender.recv_from(&mut recv_buf).expect("Can't receive timestamp back.");
+                let now = time::precise_time_ns();
+                let sent = recv_buf.as_slice().read_u64::<BigEndian>().expect("Can't parse timestamp");
 
-                // Our ECHOER is ready to be read from.
-                PONG => {
-                    let num_recv = sender.recv(&mut buffer).expect("Receiving failed");
-                    println!("received {:?} -> {:?}", buffer, num_recv);
-                    buffer = [0; 9];
-                }
-
-                _ => unreachable!()
+                wtr.serialize(Row { latency_ns: now-sent }).expect("Can't write record");
+                waiting_for_reply = false;
             }
         }
+
+        i = i + 1;
+        if i % 10000 == 0 {
+            wtr.flush().expect("Can't flush the csv log");
+        }
+
     }
+
 }
