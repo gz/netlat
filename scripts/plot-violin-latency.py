2f66e83ab0482ae0b73d60c188662b3a40a45a60
Gerd Zellweger <mail@gerdzellweger.com>
2018-04-02 18:25:58 -0700

    Update axes in script.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- scripts/plot-violin-latency.py
+++ scripts/plot-violin-latency.py
@@ -1,6 +1,6 @@
 import os, sys
 import glob
-
+import numpy
 import matplotlib
 matplotlib.use('Agg')
 
@@ -11,15 +11,20 @@ import pandas as pd
 
 if __name__ == '__main__':
 
-    fig, axes = plt.subplots(nrows=int((len(sys.argv)-1)/3), ncols=3, figsize=(10,10))
+    fig, axes = plt.subplots(nrows=int( (len(sys.argv))/3), ncols=3, figsize=(10,10))
+
+    min_all = 10**10
+    max_all = 0
 
     for i, f in enumerate(sys.argv[1:]):
         filename, file_extension = os.path.splitext(os.path.basename(f))
-
         raw_data = pd.read_csv(f, skipinitialspace=True)
         series = raw_data['latency_ns']
         rseries = pd.Series(series)
-
+        if min_all > rseries.min():
+            min_all = rseries.min()
+        if max_all < rseries.max():
+            max_all = rseries.max()
 
         axes[int(i/3), i%3].set_ylabel('Latency [ns]')
         axes[int(i/3), i%3].set_xlabel('')
@@ -31,9 +36,12 @@ if __name__ == '__main__':
 
         axes[int(i/3), i%3].violinplot(rseries, points=20, widths=0.1, showmeans=True, showextrema=True, showmedians=True)
         axes[int(i/3), i%3].set_title(filename.split("-")[1])
+    
+    for (m,n), subplot in numpy.ndenumerate(axes):
+        subplot.set_ylim(min_all,max_all)
 
     plt.legend()
-    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.4, hspace=None)
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.40, hspace=0.2)
 
-    plt.savefig("plot-violin-latency.png", format='png')
-    plt.savefig("plot-violin-latency.pdf", format='pdf')
+    plt.savefig("plot-violin-latency.png", format='png', pad_inches=0.0)
+    plt.savefig("plot-violin-latency.pdf", format='pdf', pad_inches=0.0)
