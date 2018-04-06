24692e6f7d0fdee6414924263beb9e2a0a648f39
Gerd Zellweger <mail@gerdzellweger.com>
2018-04-06 15:56:45 -0700

    Update scresser script
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- scripts/stresser.py
+++ scripts/stresser.py
@@ -2,16 +2,18 @@
 
 import time
 import math
+import sys
 
-percent_cpu = 70
+if __name__ == '__main__':
+    percent_cpu = sys.argv[1]
 
-time_of_run = 0.1
-cpu_time_utilisation = float(percent_cpu) / 100
-on_time = time_of_run * cpu_time_utilisation
-off_time = time_of_run * (1-cpu_time_utilisation)
+    time_of_run = 0.1
+    cpu_time_utilisation = float(percent_cpu) / 100
+    on_time = time_of_run * cpu_time_utilisation
+    off_time = time_of_run * (1-cpu_time_utilisation)
 
-while True:
-    start_time = time.clock()
-    while time.clock() - start_time < on_time:
-        math.factorial(100)
-    time.sleep(off_time)
+    while True:
+        start_time = time.clock()
+        while time.clock() - start_time < on_time:
+            math.factorial(100)
+        time.sleep(off_time)
